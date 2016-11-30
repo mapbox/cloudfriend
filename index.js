d7029534c275a24152662dd45e659abf9b31bf58
Ryan Clark <ryan.clark.j@gmail.com>
2016-11-30 16:10:54 -0700

    export a template for ci permissions to perform template validation

--- index.js
+++ index.js
@@ -28,3 +28,38 @@ Object.keys(conditions).forEach(function(key) {
 Object.keys(pseudo).forEach(function(key) {
   cloudfriend[key] = pseudo[key];
 });
+
+cloudfriend.permissions = {
+  AWSTemplateFormatVersion: '2010-09-09',
+  Resources: {
+    User: {
+      Type: 'AWS::IAM::User',
+      Properties: {
+        Policies: [
+          {
+            PolicyName: 'validate-templates',
+            PolicyDocument: {
+              Statement: [
+                {
+                  Action: 'cloudformation:ValidateTemplate',
+                  Effect: 'Allow',
+                  Resource: '*'
+                }
+              ]
+            }
+          }
+        ]
+      }
+    },
+    AccessKey: {
+      Type: 'AWS::IAM::AccessKey',
+      Properties: {
+        UserName: cloudfriend.ref('User')
+      }
+    }
+  },
+  Outputs: {
+    AccessKeyId: { Value: cloudfriend.ref('AccessKey') },
+    SecretAccessKey: { Value: cloudfriend.getAtt('AccessKey', 'SecretAccessKey') }
+  }
+};
