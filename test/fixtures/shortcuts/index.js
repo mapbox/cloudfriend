a3851c0d2c86820788cb47fa8796b76a79ec9000
Ryan Clark <ryan.clark.j@gmail.com>
2018-08-01 14:41:26 -0700

    shortcut test suite

---
+++ test/fixtures/shortcuts/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+  update: (name, data) => {
+    fs.writeFileSync(path.join(__dirname, `${name}.json`), JSON.stringify(data, null, 2));
+  },
+
+  get: (name) => {
+    return require(path.join(__dirname, `${name}.json`));
+  }
+};
