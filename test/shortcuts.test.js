414ead8266b7eb500e68b0345be23ab69858b5f9
Ryan Clark <ryan.clark.j@gmail.com>
2019-10-01 13:57:05 -0700

    don't set parameters on orc tables

--- test/shortcuts.test.js
+++ test/shortcuts.test.js
@@ -851,7 +851,7 @@ test('[shortcuts] glue json table', (assert) => {
   assert.end();
 });
 
-test('[shortcuts] glue orc table', (assert) => {
+test.only('[shortcuts] glue orc table', (assert) => {
   assert.throws(
     () => new cf.shortcuts.GlueOrcTable(),
     /You must provide a Location/,
