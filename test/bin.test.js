11f24c88175c46d27161c4039b9f4e5f758880f7
theborowski <theborowski@gmail.com>
2026-02-06 13:59:58 -0700

    use a slightly older version of eslint-config-mapbox & address copilot comment

--- test/bin.test.js
+++ test/bin.test.js
@@ -1,19 +1,23 @@
 'use strict';
 
 const path = require('path');
-const { exec } = require('child_process');
+const { execFile } = require('child_process');
 const { promisify } = require('util');
 
-const execAsync = promisify(exec);
+const execFileAsync = promisify(execFile);
 
 describe('bin/build-template', () => {
   const script = path.normalize(__dirname + '/../bin/build-template.js');
   const template = path.normalize(__dirname + '/fixtures/sync-args.js');
 
   test('outputs expected', async () => {
-    const { stdout, stderr } = await execAsync(
-      [script, template, '--this', 'that', '-r', 'cn-north-1'].join(' ')
-    );
+    const { stdout, stderr } = await execFileAsync(script, [
+      template,
+      '--this',
+      'that',
+      '-r',
+      'cn-north-1'
+    ]);
     expect(stderr).toBeFalsy();
     const result = JSON.parse(stdout);
     expect(result.this).toBe('that');
